7f17aa200607bf0c9c55b96e234b9b6394f1a328
amdiutama <77255478+amdiutama@users.noreply.github.com>
2021-05-19 23:29:00 +0700

    Delete session.py

--- lib/session.py
+++
@@ -1,8 +0,0 @@
-import requests 
-ses = requests.Session()
-from bs4 import BeautifulSoup as parser
-class browser:
-    def __init__(self,kuki):
-        self.kuki = {"cookie":kuki}
-    def get(self,link):
-        return parser(ses.get("https://free.facebook.com" + link,cookies=self.kuki).content,"html.parser")
